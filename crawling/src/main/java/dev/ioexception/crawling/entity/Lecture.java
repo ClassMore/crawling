47240672e21b7743df9884af08e58af6bf3d0bd3
BHC-Chicken <tij05078@gmail.com>
2024-07-10 15:34:59 +0900

    [refactoring] import 변경

--- crawling/src/main/java/dev/ioexception/crawling/entity/Lecture.java
+++ crawling/src/main/java/dev/ioexception/crawling/entity/Lecture.java
@@ -1,10 +1,5 @@
 package dev.ioexception.crawling.entity;
 
-import jakarta.persistence.FetchType;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,13 +7,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 
 @Entity
